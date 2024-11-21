b999b5e7bc147158cae445deea3844952979c9ac
hemarydem <hamedremy@hotmail.fr>
2024-11-21 15:54:35 +0100

    wip: animation header for small laptop ok - update homepage - update css for home page responsive for small laptop

--- src/presentation/pages/create-home-page.tsx
+++ src/presentation/pages/create-home-page.tsx
@@ -1,5 +1,8 @@
-import { FooterComponent } from "../components/footer-component/footer-component"
-import { HeaderComponent } from "../components/header-component/header-component"
+import { FooterComponent } from "../components/footer-component/footer-component";
+import { HeaderComponent } from "../components/header-component/header-component";
+import logo_linkedin  from "/src/assets/socialmedia/linkedin-logo-linkedin-icon-transparent-free-png.webp";
+import git_hub_logo from "/src/assets/socialmedia/png-transparent-github-computer-icons-github-logo-monochrome-head-thumbnail.png";
+import x_logo from "/src/assets/socialmedia/téléchargement.jpg";
 import "./style.scss";
 
 export const CreateHomePage: React.FC = () => {
@@ -19,69 +22,69 @@ export const CreateHomePage: React.FC = () => {
                     <div className="div-2">
                         <div>
                             <div>
-                                <a href="https://www.linkedin.com/in/rhamed/"  target="_blank">
-                                    <img src="assets/socialmedia/linkedin-logo-linkedin-icon-transparent-free-png.webp"/>
+                                <a href="https://www.linkedin.com/in/rhamed/"   target="_blank">
+                                    <img src={logo_linkedin} alt="Linkedin logo" />
                                 </a>
                                 <a href="https://github.com/remyHamed"  target="_blank">
-                                    <img src="assets/socialmedia/png-transparent-github-computer-icons-github-logo-monochrome-head-thumbnail.png" alt=""/>
+                                    <img src={git_hub_logo} alt="gitHub logo"/>
                                 </a>
                                 <a href="https://x.com/dev_remy_"  target="_blank">
-                                    <img src="assets/socialmedia/téléchargement.jpg"/>
+                                    <img src={x_logo} alt="X logo"/>
                                 </a>
                             </div>
                         </div>
                     </div>
-                </div>  
-                <div className="div-1">
-                    <div className="text-container">
-                        <h1>Expériences professionnelles</h1>
-                        <div >
-                            <h2>
-                                Developpeur full stack
-                                Entreprise Juridifeel
-                            </h2>
-                            <h3>
-                                Septembre 2021 - Mars 2023
-                                Angular 16 /Symfony 5.5 (php 8, php 7.2.5)
-                            </h3>
-                            <p>
-                                Collecte des Besoins, établissement du cahier des charges, proposition de Solutions
-                                Techniques :En tant que membre d'une start-up innovante, j'ai initié le projet dès ses prémices, en
-                                étant en contact direct avec le fondateur, utilisateur de l'application. Cette interaction m'a permis
-                                de saisir ses attentes et les exigences spécifiques du projet. J'ai activement participé à des sessions
-                                de discussion et de brainstorming pour recueillir des besoins détaillés. Ensuite, j'ai proposé des
-                                solutions techniques adaptées, assurant que l'application répondait parfaitement aux objectifs de
-                                l'entreprise.
-                            </p>
-                            <p>
-                                Collecte des Besoins, établissement du cahier des charges, proposition de Solutions
-                                Techniques :En tant que membre d'une start-up innovante, j'ai initié le projet dès ses prémices, en
-                                étant en contact direct avec le fondateur, utilisateur de l'application. Cette interaction m'a permis
-                                de saisir ses attentes et les exigences spécifiques du projet. J'ai activement participé à des sessions
-                                de discussion et de brainstorming pour recueillir des besoins détaillés. Ensuite, j'ai proposé des
-                                solutions techniques adaptées, assurant que l'application répondait parfaitement aux objectifs de
-                                l'entreprise.
-                            </p>
-                            <p>
-                                Collecte et Centralisation des Données : Développement de fonctionnalités permettant de
-                                recueillir des données via des API et de les centraliser dans une vue unique, fournissant des
-                                informations exhaustives sur les entreprises.
-                            </p>
-                            <p>
-                                Automatisation du Travail Juridique : Développement de fonctionnalités pour automatiser le
-                                travail des juristes en gérant et en auto-complétant la documentation basée sur les informations
-                                collectées, facilitant ainsi la relecture et la validation par les juristes.
-                            </p>
-                            <p>
-                                Dans mon travail, j'ai été proactif en conseillant l'utilisation d'une base de données MongoDB
-                                orientée document pour améliorer la performance. J'ai initié la transition de notre API vers les
-                                standards REST pour une meilleure interopérabilité et intégration. J'ai structuré notre workflow en
-                                proposant le développement agile et mené des actions de formation et sensibilisation pour
-                                aligner l'équipe sur ces nouvelles pratiques, y compris la relecture de code et le pair programming.
-                            </p>
+                    <div className="div-1">
+                        <div className="text-container">
+                            <h1>Expériences professionnelles</h1>
+                            <div >
+                                <h2>
+                                    Developpeur full stack
+                                    Entreprise Juridifeel
+                                </h2>
+                                <h3>
+                                    Septembre 2021 - Mars 2023
+                                    Angular 16 /Symfony 5.5 (php 8, php 7.2.5)
+                                </h3>
+                                <p>
+                                    Collecte des Besoins, établissement du cahier des charges, proposition de Solutions
+                                    Techniques :En tant que membre d'une start-up innovante, j'ai initié le projet dès ses prémices, en
+                                    étant en contact direct avec le fondateur, utilisateur de l'application. Cette interaction m'a permis
+                                    de saisir ses attentes et les exigences spécifiques du projet. J'ai activement participé à des sessions
+                                    de discussion et de brainstorming pour recueillir des besoins détaillés. Ensuite, j'ai proposé des
+                                    solutions techniques adaptées, assurant que l'application répondait parfaitement aux objectifs de
+                                    l'entreprise.
+                                </p>
+                                <p>
+                                    Collecte des Besoins, établissement du cahier des charges, proposition de Solutions
+                                    Techniques :En tant que membre d'une start-up innovante, j'ai initié le projet dès ses prémices, en
+                                    étant en contact direct avec le fondateur, utilisateur de l'application. Cette interaction m'a permis
+                                    de saisir ses attentes et les exigences spécifiques du projet. J'ai activement participé à des sessions
+                                    de discussion et de brainstorming pour recueillir des besoins détaillés. Ensuite, j'ai proposé des
+                                    solutions techniques adaptées, assurant que l'application répondait parfaitement aux objectifs de
+                                    l'entreprise.
+                                </p>
+                                <p>
+                                    Collecte et Centralisation des Données : Développement de fonctionnalités permettant de
+                                    recueillir des données via des API et de les centraliser dans une vue unique, fournissant des
+                                    informations exhaustives sur les entreprises.
+                                </p>
+                                <p>
+                                    Automatisation du Travail Juridique : Développement de fonctionnalités pour automatiser le
+                                    travail des juristes en gérant et en auto-complétant la documentation basée sur les informations
+                                    collectées, facilitant ainsi la relecture et la validation par les juristes.
+                                </p>
+                                <p>
+                                    Dans mon travail, j'ai été proactif en conseillant l'utilisation d'une base de données MongoDB
+                                    orientée document pour améliorer la performance. J'ai initié la transition de notre API vers les
+                                    standards REST pour une meilleure interopérabilité et intégration. J'ai structuré notre workflow en
+                                    proposant le développement agile et mené des actions de formation et sensibilisation pour
+                                    aligner l'équipe sur ces nouvelles pratiques, y compris la relecture de code et le pair programming.
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div>  
             </main>
             <FooterComponent/>
         </>
