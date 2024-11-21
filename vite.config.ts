8d6bf88273c61ea41f4dbdc3d4e666cea87bc000
hemarydem <hamedremy@hotmail.fr>
2024-11-21 17:31:07 +0100

    fix; defin config to deply

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/remy-portefolio-v2/",
   resolve: {
     alias: {
       '@': '/src',
