f8a9f156d64d74417d66a62472707f55ea44fff3
hemarydem <hamedremy@hotmail.fr>
2024-12-18 13:30:44 +0100

    wip: - new back ground testing - education and training wip

--- src/presentation/pages/create-education-and-training/create-education-and-training.tsx
+++ src/presentation/pages/create-education-and-training/create-education-and-training.tsx
@@ -1,71 +1,87 @@
+import { useState } from "react";
 import { FooterComponent } from "../../../presentation/components/footer-component/footer-component";
 import { HeaderComponent } from "../../../presentation/components/header-component/header-component";
 
 export const CreateEducationAndTrainingPage: React.FC = ():React.ReactNode => {
+    
+    const [showlinks, setShowlinks] = useState(false);
+
+    const handleShowLinks = () => {
+        setShowlinks((showlinks) => !showlinks);
+    }
     return(
         <>
-        <HeaderComponent />
-            <main>
-                <div className="container-">
-                    <div className="div-1">
-                        <div className="text-container-education-and-training">
-                            <h1>Master Architecture des Logiciels</h1>
-                            <div className="progresse-bar-container">
-                                <div className="progress-bar"></div>
+            <HeaderComponent showLinks={showlinks} />
+            <main className="container-home" >
+            <div className="navbar_burger" onClick={handleShowLinks}>
+                    <div className="burger-bar"></div>
+                    <div className="burger-bar"></div>
+                    <div className="burger-bar"></div>
+                </div>
+                <div>
+                    <h3 className="title-home">Rémy Hamed</h3>
+                </div>
+                <div  className="">
+                    <div className="home-div-container">
+                        <div>
+                            <div className="text-container-education-and-training">
+                                <h4>Master Architecture des Logiciels</h4>
+                                <div className="progress-bar-container">
+                                    <div className="progress-bar"></div>
+                                </div>
+                                <h5>
+                                    De septembre 2022 à septembre 2024
+                                </h5>
+                                <h5>
+                                    Ecole Supérieure de Génie Informatique
+                                </h5>
                             </div>
-                            <h3>
-                                De septembre 2022 à septembre 2024
-                            </h3>
-                            <h3>
-                                Ecole Supérieure de Génie Informatique
-                            </h3>
                         </div>
-                    </div>
-                    <div className="div-1">
-                        <div className="text-container">
-                            <h1>Bachelor chef de projet logiciel et réseau</h1>
-                            <div className="progresse-bar-container">
-                                <div className="progress-bar"></div>
+                        <div>
+                            <div className="text-container-education-and-training">
+                                <h1>Bachelor chef de projet logiciel et réseau</h1>
+                                <div className="progress-bar-container">
+                                    <div className="progress-bar"></div>
+                                </div>
+                                <h3>
+                                    De septembre 2019 à septembre 2022
+                                </h3>
+                                <h3>
+                                    Ecole Supérieure de Génie Informatique
+                                </h3>
                             </div>
-                            <h3>
-                                De septembre 2019 à septembre 2022
-                            </h3>
-                            <h3>
-                                Ecole Supérieure de Génie Informatique
-                            </h3>
                         </div>
-                    </div>
-                    <div className="div-1">
-                        <div className="text-container">
-                            <h1>Licence histoire</h1>
-                            <div className="progresse-bar-container">
-                                <div className="progress-bar"></div>
+                        <div>
+                            <div className="text-container-education-and-training">
+                                <h1>Licence histoire</h1>
+                                <div className="progress-bar-container">
+                                    <div className="progress-bar"></div>
+                                </div>
+                                <h3>
+                                    De septembre 2013 à juillet 2016 
+                                </h3>
+                                <h3>
+                                    Nanterre Paris Ouest Nanterre
+                                </h3>
                             </div>
-                            <h3>
-                                De septembre 2013 à juillet 2016 
-                            </h3>
-                            <h3>
-                                Nanterre Paris Ouest Nanterre
-                            </h3>
                         </div>
-                    </div>
-                    <div className="div-1">
-                        <div className="text-container">
-                            <h1>Baccalauréat science et technologie de la gestion option communication</h1>
-                            <div className="progresse-bar-container">
-                                <div className="progress-bar"></div>
+                        <div>
+                            <div className="text-container-education-and-training">
+                                <h1>Baccalauréat science et technologie de la gestion option communication</h1>
+                                <div className="progress-bar-container">
+                                    <div className="progress-bar"></div>
+                                </div>
+                                <h3>
+                                    De septembre 2010 à septembre 2011
+                                </h3>
+                                <h3>
+                                    EREA Toulouse Lautrec
+                                </h3>
                             </div>
-                            <h3>
-                                De septembre 2010 à septembre 2011
-                            </h3>
-                            <h3>
-                                EREA Toulouse Lautrec
-                            </h3>
                         </div>
                     </div>
                 </div>
             </main>
-            <FooterComponent/>
         </>
     );
 };
--- src/presentation/pages/create-home-page/create-home-page.tsx
+++ src/presentation/pages/create-home-page/create-home-page.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { FooterComponent } from "../../components/footer-component/footer-component";
 import { HeaderComponent } from "../../components/header-component/header-component";
 import "./style.scss";
 
@@ -21,10 +20,34 @@ export const CreateHomePage: React.FC = () :React.ReactNode => {
                     <div className="burger-bar"></div>
                 </div>
                 <div>
-                    <h3>Rémy Hamed</h3>
+                    <h3 className="title-home">Rémy Hamed</h3>
+                </div>
+                <div  className="home-div">
+                    <div className="home-div-container">
+                        <h3>Développeur full stack</h3>
+                        <p className="home-paragraphe">
+                            Passionné par la conception de solutions logicielles innovantes, je suis diplômé d’un master en architecture logicielle à l’E.S.G.I. Mon objectif est de contribuer à des projets ambitieux tout en enrichissant mes compétences.
+                        </p>
+                        <div className="home-list">
+                            <p>
+                                Expertise full stack, de la création d’interfaces à la mise en place de structures backend.
+                            </p>
+                            <p>
+                                Expérience dans l’évolution d’architectures logicielles avec MongoDB et des API REST.
+                            </p>
+                            <p>
+                                Développement de fonctionnalités pour automatiser et optimiser des processus.
+                            </p>
+                            <p>
+                                Capacité à mener des projets techniques complexes avec rigueur et créativité.
+                            </p>
+                            <p>
+                                Développement de fonctionnalités pour automatiser et optimiser des processus.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </main>
-            <FooterComponent/>
         </>
     );
 }
