99f2ceccaf9112707fa443c78630ac4c0a13d072
hemarydem <hamedremy@hotmail.fr>
2024-12-27 17:26:27 +0100

    fix; item - 5 title ok

--- src/presentation/pages/create-home-page/create-home-page.tsx
+++ src/presentation/pages/create-home-page/create-home-page.tsx
@@ -139,6 +139,105 @@ export const CreateHomePage: React.FC = () :React.ReactNode => {
                         <button className="resume-button-item-4">Télécharger mon cv</button>
                     </div>
                 </div>
+                <div className="item-3">
+                    <h1 className="item-3-back">Services</h1>
+                    <div className="item-3-front">
+                        <h1 className="item-3-title">Services</h1>
+                        <p  className="item-3-front-paragraphe">
+                            A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+                        </p>
+                    </div>
+                </div>
+                <div className="item-5">
+                    <div className="row">
+                        <div className="square">
+                            <h2 className="year-diploma">
+                                Développement Web
+                            </h2>
+                            <h1 className="title-diploma">
+                                Master d'architecture logiciel, Informatique
+                            </h1>
+                            <h4 className="school">
+                                École Supérieure de Génie Informatique
+                            </h4>
+                            <p className="item-4-pargraphe">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dolores quas, tenetur accusantium facilis delectus magni nobis vero. Maiores non expedita magni ab culpa, vero consequatur natus soluta eum ratione.
+                            </p>
+                        </div>
+                        <div className="square">
+                            <h2 className="year-diploma">
+                                2021-2022
+                            </h2>
+                            <h1 className="title-diploma">
+                                Bachelor chef de projet logiciel et réseau, Informatique
+                            </h1>
+                            <h4 className="school">
+                                École Supérieure de Génie Informatique
+                            </h4>
+                            <p className="item-4-pargraphe">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dolores quas, tenetur accusantium facilis delectus magni nobis vero. Maiores non expedita magni ab culpa, vero consequatur natus soluta eum ratione.
+                            </p>
+                        </div>
+                        <div className="square">
+                            <h2 className="year-diploma">
+                                2021-2022
+                            </h2>
+                            <h1 className="title-diploma">
+                                Bachelor chef de projet logiciel et réseau, Informatique
+                            </h1>
+                            <h4 className="school">
+                                École Supérieure de Génie Informatique
+                            </h4>
+                            <p className="item-4-pargraphe">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dolores quas, tenetur accusantium facilis delectus magni nobis vero. Maiores non expedita magni ab culpa, vero consequatur natus soluta eum ratione.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="square">
+                            <h2 className="year-diploma">
+                                2013-2016
+                            </h2>
+                            <h1 className="title-diploma">
+                                Licence histoire
+                            </h1>
+                            <h4 className="school">
+                                Nanterre Paris Ouest
+                            </h4>
+                            <p className="item-4-pargraphe">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dolores quas, tenetur accusantium facilis delectus magni nobis vero. Maiores non expedita magni ab culpa, vero consequatur natus soluta eum ratione.
+                            </p>
+                        </div>
+                        <div className="square">
+                            <h2 className="year-diploma">
+                                2010-2011
+                            </h2>
+                            <h1 className="title-diploma">
+                                Baccalauréat science et technologie de la gestion option communication
+                            </h1>
+                            <h4 className="school">
+                                EREA Toulouse Lautrec
+                            </h4>
+                            <p className="item-4-pargraphe">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dolores quas, tenetur accusantium facilis delectus magni nobis vero. Maiores non expedita magni ab culpa, vero consequatur natus soluta eum ratione.
+                            </p>    
+                        </div>
+                        <div className="square">
+                            <h2 className="year-diploma">
+                                2021-2022
+                            </h2>
+                            <h1 className="title-diploma">
+                                Bachelor chef de projet logiciel et réseau, Informatique
+                            </h1>
+                            <h4 className="school">
+                                École Supérieure de Génie Informatique
+                            </h4>
+                            <p className="item-4-pargraphe">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dolores quas, tenetur accusantium facilis delectus magni nobis vero. Maiores non expedita magni ab culpa, vero consequatur natus soluta eum ratione.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     );
