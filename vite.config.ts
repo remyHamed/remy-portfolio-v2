2cae97df84e04cee3be1fe3d8a63505ff574511d
hemarydem <hamedremy@hotmail.fr>
2025-06-05 13:05:02 +0200

    fix: path error on index html - modify setting vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import react from "@vitejs/plugin-react";
-<<<<<<< Updated upstream
-=======
 import { defineConfig } from "vite";
->>>>>>> Stashed changes
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/remy-portefolio-v2",
-<<<<<<< Updated upstream
-=======
   build: {
     outDir: "dist",
     rollupOptions: {
@@ -21,7 +16,6 @@ export default defineConfig({
       },
     },
   },
->>>>>>> Stashed changes
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
