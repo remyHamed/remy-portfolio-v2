8f8ebe852c0d6f55eea4b0a151e753a96bc892e9
hemarydem <hamedremy@hotmail.fr>
2025-06-05 12:45:42 +0200

    wip: fixing to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+<<<<<<< Updated upstream
+=======
+import { defineConfig } from "vite";
+>>>>>>> Stashed changes
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/remy-portefolio-v2",
+<<<<<<< Updated upstream
+=======
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
+>>>>>>> Stashed changes
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
